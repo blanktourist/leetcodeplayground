5b4a19b49a33285645168a90426dc2b43c5c68a7
blanktourist <101967933+blanktourist@users.noreply.github.com>
2022-05-07 20:35:18 -0700

    Add PriorityQueueTesting.java

---
+++ src/PriorityQueueTesting.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class PriorityQueueTesting {
+    
+    public static void main(String[] args) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> b[0] - a[0]);
+        int[] arr1 = new int[]{1,0};
+        int[] arr2 = new int[]{2,0};
+        int[] arr3 = new int[]{3,0};
+
+        pq.add(arr3);
+        pq.add(arr2);
+        pq.add(arr1);
+
+        System.out.println(Arrays.toString(pq.poll()));
+        System.out.println(Arrays.toString(pq.poll()));
+        System.out.println(Arrays.toString(pq.poll()));
+    }
+}
